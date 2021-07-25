d7e1983509a94e7d1d6d593dc0c3c21742a31642
tanner <tannersimpkins@gmail.com>
2021-07-25 14:53:31 -0500

    Added Slider component file and placeholder

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Slider from './Slider';
 
 const Landing = () => {
   return (
     <section className='landing'>
       <div className='dark-overlay'>
-        <div className='landing-inner' />
+        <div className='landing-inner'>
+          <Slider />
+        </div>
       </div>
     </section>
   );
---
+++ client/src/components/layout/Slider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slider = () => {
+    return (
+        <div>
+            Slider here
+        </div>
+    )
+}
+
+export default Slider
